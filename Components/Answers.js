53e7f3d549ccd1ae4f24c036ebe73637880dc378
rudy35hernandez <67667227+rudy35hernandez@users.noreply.github.com>
2022-07-13 00:00:01 -0700

    added classname with ternary to make correct classname selection

--- Components/Answers.js
+++ Components/Answers.js
@@ -4,6 +4,13 @@ const [isHovering, setIsHovering] = React.useState(false);
 
 function Answers(){
 
+    const className = props.allChecked ?
+                props.isCorrect ?
+                    'correct' : props.isSelected ?
+                    'wrong' : 'checked' :
+                        props.isSelected ?
+                            'selected' : 'answer'
+
 }
 
 export default Answers
