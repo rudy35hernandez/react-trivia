ade66e912a78d29dbe09522e3bef4730bb27744b
rudy35hernandez <67667227+rudy35hernandez@users.noreply.github.com>
2022-07-21 00:09:53 -0700

    added shuffle method

--- App.js
+++ App.js
@@ -52,6 +52,16 @@ function App(){
         return allAnswers
     }
     
+
+    function shuffleArray(array) {
+        for (var i = array.length - 1; i > 0; i--) {
+            var j = Math.floor(Math.random() * (i + 1));
+            var temp = array[i];
+            array[i] = array[j];
+            array[j] = temp;
+        }
+        return array
+    }
     
     function newQuestions(data){
         const questions = data.map(el => {
