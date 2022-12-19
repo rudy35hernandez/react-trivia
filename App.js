1a9a80dbb0dded510a629e8ca62bb4c31670d6c8
rudy35hernandez <67667227+rudy35hernandez@users.noreply.github.com>
2022-12-18 23:47:23 -0800

    added new method to grab id of answers user is clicking

--- App.js
+++ App.js
@@ -2,14 +2,18 @@ import React from 'react'
 import Opening from "./Opening.js"
 import GamePage from "./GamePage.js"
 import Question from "./Question.js"
+import Answers from "./Answers.js"
+import IncorrectAnswer from "./IncorrectAnswer.js"
 
 function App(){
     
     const [startGame, setStartGame] = React.useState(false)
     const [apiReturn, setApiReturn] = React.useState([])
+    const [select, setSelect] = React.useState(false)
+    
     
     React.useEffect(() => {
-    fetch(`https://opentdb.com/api.php?amount=5&category=21&difficulty=easy&type=multiple`)
+    fetch(`https://opentdb.com/api.php?amount=5&type=multiple`)
         .then(res => res.json())
         .then(data => setApiReturn(data.results))
     }, [])
@@ -27,22 +31,43 @@ function escapeHTML(str){
     }[tag]))
 }
 
-console.log(escapeHTML(`In what sport is a &quot;shuttlecock&quot; used?`))
+function checkId(id){
+    console.log(id)
+}
 
-    function apiEl(){
-        return apiReturn.map(api => {
-            return (
+
+
+
+  const questionEl = apiReturn.map(api => {
+    //   console.log(api)
+//    console.log(api.incorrect_answers)
+   
+let a = api.incorrect_answers
+
+let b = a.map((str, index) => ({id: Math.random() * 100, value: str}))
+
+// console.log(b);
+ 
+       return (
+           <div>
                 <Question 
-                    question={api.question}
+                    question={api.question} 
+                /> 
+                <Answers 
+                    answer={api.correct_answer}
                 />
+                <IncorrectAnswer
+                    incorrectTest={b}
+                    checkId={checkId}
+                    id={b.id}
+                />
+            </div>
             )
         })
-    }
-    
-    
-    
-    
-    
+
+
+
+ 
     function clickStart(){
         setStartGame(true)
     }
@@ -57,8 +82,9 @@ console.log(escapeHTML(`In what sport is a &quot;shuttlecock&quot; used?`))
         
         :
        <div>
-        {apiEl()}
+         {questionEl}
        </div>
+    
         
     )
 }
